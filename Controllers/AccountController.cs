8f1622a556d5bf6f4cc23d6e67bf3ff1b41f7f53
Konrad Milewski <konradmilewski2000@interia.eu>
2022-05-20 12:25:12 +0200

    Dodaj pliki projektów.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace BalanceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AccountController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            
+
+            return null;
+        }
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BalanceAPI.Models
+{
+    public class Account
+    {
+        [Key]
+        public int AccountId { get; set; }
+
+        public string Name { get; set; };
+
+    }
+}
